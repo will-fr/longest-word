8023ae998a0884cf35a2c38bee874f94578ed976
William LIEVIN <william.lievin@gmail.com>
2020-12-15 16:13:21 +0100

    Feature complete: Dictionary check of attempt

--- game.py
+++ game.py
@@ -3,23 +3,38 @@
 # pylint: disable=too-few-public-methods
 import random
 import string
+import requests
+
+
 
 class Game:
     def __init__(self):
         self.grid = []
         self.player_word=""
+        self.playing=True
+
+
         for _ in range (9):
             self.grid.append(random.choice(string.ascii_uppercase))
 
     def start_game(self):
         print ("WELCOME TO THE GAME ! ")
         print (f"The grid is : {self.grid}")
-        while not self.is_valid(self.player_word):
+        while self.playing:
             self.player_word = input("What is your word ? ")
             print (f"Your Word is : {self.player_word}")
 
+            if not self.is_valid(self.player_word):
+                print ("Word not ok !")
+                continue
+
+            print ("Your word is valid ! ")
+            playing = False
+
+
     def is_valid(self, word):
         if not word:
+            print ("<err>Entrez un mot !")
             return False
 
         letters = self.grid.copy()
@@ -27,9 +42,16 @@ class Game:
             if letter in letters:
                 letters.remove(letter)
             else:
+                print ("<err>lettres incorrectes")
                 return False
-        return True
 
+
+        return self.is_in_dictionary(word)
+
+    def is_in_dictionary(self, word):
+        r = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = r.json()
+        return json_response['found']
 
 def main():
     print ("starting...")
