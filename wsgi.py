7e75d99b31644139676f78cbb8b0f96b73a03975
William LIEVIN <william.lievin@gmail.com>
2020-12-16 12:32:57 +0100

    Procfile

--- wsgi.py
+++ wsgi.py
@@ -18,3 +18,5 @@ def check():
     word = request.form['word']
     is_valid = game.is_valid(word)
     return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+
+#adding comment
